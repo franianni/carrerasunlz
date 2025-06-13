377eaf9aaca5d0871245484e64511a28ef0a3cca
franianni <167141550+franianni@users.noreply.github.com>
2025-06-13 11:41:45 -0300

    Update carreras.py

--- carreras.py
+++ carreras.py
@@ -1,12 +1,13 @@
 import streamlit as st
 
-# Datos dummy para ejemplo
 facultades = {
-    "Derecho": ["Abogacía", "Notariado"],
-    "Cs. Sociales": ["Comunicación Social", "Trabajo Social"],
-    "Cs. Económicas": ["Contador Público", "Economía"],
-    "Cs. Agrarias": ["Agronomía", "Medicina Veterinaria"],
-    "Ingeniería": ["Ingeniería Civil", "Ingeniería en Sistemas"]
+    "Facultad de Derecho": ["Abogacía", "Ciclos de Complementación Curricular:", "*Licenciatura en Seguridad", "*Licenciatura en Comercio Internacional y Aduanas"],
+    "Facultad de Cs. Sociales": ["Ciencias de la Educación (Licenciatura y Profesorado)", "Comunicación Social (Licenciatura y Profesorado)", "Letras (Licenciatura y Profesorado)", "Minoridad y Familia (Tecnicatura)", "Periodismo (Tecnicatura y Licenciatura)", "Psicopedagogía (Licenciatura y Profesorado)", "Publicidad (Tecnicatura y Licenciatura)", "Relaciones Laborales (Tecnicatura y Licenciatura)", "Relaciones Públicas (Tecnicatura y Licenciatura)", "Trabajo Social (Licenciatura)"],
+    "Facultad de Cs. Económicas": ["Contador Público", "Licenciado en Administración"],
+    "Facultad de Cs. Agrarias": ["Ingeniería Agronómica", "Ingeniería Zootecnista", "Técnico Universitario en Procesamiento Agroalimentario", "Técnico Universitario en Producción Vegetal", "Técnico Universitario en Producción Animal", "Técnico Universitario en Diseño y Mantenimiento de Espacios Verdes", "Técnico Universitario en Arboricultura y Vivericultura
+"Tecnicatura Universitaria en Agroecología"],
+    "Facultad de Ingeniería": ["Ingeniería Industrial", "Ingeniería Industrial", "Ingeniería Mecátrónica", "Ingeniería Ferroviaria", "Ingeniería Mecánica", "Licenciatura en Higiene y Seguridad en el Trabajo*", "Licenciatura en Gestión de Sistemas de Automatización y Robótica*", "Licenciatura en Gestión de la Información*", "Licenciatura en Enseñanza de la Matemática*"
+, "*Se cursan en sedes"]
 }
 
 st.title("Buscador de Carreras - UNLZ")
